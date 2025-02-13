c62aa1fb538742c36694e9882bce7fe94c474930
cleavon <cleavon.design.com>
2025-02-14 00:31:58 +0100

    psycopg2 & postgresql build

---
+++ app/app/test.py
@@ -0,0 +1,19 @@
+"""
+simple tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class calcTests(SimpleTestCase):
+    """Test the calc module"""
+    
+    def test_add_numbers(self):
+        """Test the addition function"""
+        res = calc.add(2, 3)
+        self.assertEqual(res, 5)
+        
+    def test_subtract_numbers(self):
+        """Test the subtraction numbers"""
+        res = calc.subtract(5, 3)
+        self.assertEqual(res, 2)
